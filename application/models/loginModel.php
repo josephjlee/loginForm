bc14f5a1d789cd06fc5cfb5bb7fd3cf5cced8cc9
mohamed hussein <m.hussein1389@gmail.com>
2017-07-04 15:22:44 +0300

    add addUser function

--- application/models/loginModel.php
+++ application/models/loginModel.php
@@ -13,7 +13,7 @@ class LoginModel extends CI_Model
      */
     public function checkLogin($data)
     {
-        $userName=$data['userName'];
+        $userName=$data['name'];
         $password=$data['password'];
         
         $condition ="name ='$userName' AND password='$password'";
@@ -34,5 +34,51 @@ class LoginModel extends CI_Model
             return false;
         }
     }
+    
+    /**
+     * insert new user into users tabel
+     * @param array $data username,password ,email
+     * @return boolean true if user inserted or false if already exists 
+     */
+    public function addUser($data)
+    {
+        
+         $userName=$data['name'];
+         
+         //check if user already exists
+         $condition ="name ='$userName'";
+        
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where($condition);
+        $this->limit(1);
+        
+        $query=$this->db->get();
+        
+        if($query->num_rows()>0)
+        {
+            //user already registered
+            return false;
+        }
+        else
+        {
+            //insert new user into users table
+            $this->db->insert('users',$data);
+            
+            if($this->db->affected_rows()>0)
+            {
+                return true;
+            }
+            else
+            {
+                //error occur
+                return false;
+            }
+        }
+         
+    }
+    
+    
+    
 }
 ?>

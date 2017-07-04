48f61831b1e00f91c1adaf69cb96e2bfdb5ac7dd
mohamed hussein <m.hussein1389@gmail.com>
2017-07-04 15:37:52 +0300

    add LoginController constructor function

---
+++ application/controllers/loginController.php
@@ -0,0 +1,31 @@
+<?php
+
+class LoginController extends CI_Controller
+{
+    public function __construct()
+    {
+        Parent::__construct();
+        
+        //load form helper
+        $this->load->helper('form');
+        
+        //load form validation library
+        $this->load->library('form_validation');
+        
+        //load session library
+        $this->load->library('session');
+        
+        //load login model
+        $this->load->model('loginModel');
+        
+        //load url helper
+        $this->load->helper('url');
+        
+        //load database
+        $this->load->database();
+    }
+    
+    
+    
+}
+?>
